ca593152346984cdff8aee71d3fa74c30a12d14e
samsuarr <personal.samuelsuarez@gmail.com>
2022-08-13 12:21:59 -0400

    Terminado el taller de calculo de descuentos

--- proyectoDescuentos/code.js
+++ proyectoDescuentos/code.js
@@ -1,7 +1,31 @@
-console.log('hola');
+var input = document.getElementById("discount");
+// const discount = input.innerHTML = random(10, 50);
+// const showDiscount = input.innerHTML = discount;
 
-let calcularDescuento = function(precio, descuento) 
+const originalPrice = 100;
+
+let calculateDiscount = function(price, discount)
 {
-    const precioFinal = (precio * (100 - descuento )) / 100;
-    return precioFinal
+    let finalPrice;
+    finalPrice  = (price * (100 - discount)) / 100;
+    
+
+    result.innerText = "El precio del producto final es $" + finalPrice;
 }
+
+
+
+function onClickButtonPriceDiscount()
+{
+    const inputPrice = document.getElementById('inputPrice');
+    const inputDiscount = document.getElementById('inputDiscount');
+
+    const price = inputPrice.value;
+    const discount = inputDiscount.value;
+   
+
+    calculateDiscount(price, discount);
+
+    // result.innetText = "El precio con descuento son $" + calculateDiscount(price, discount); 
+}
+
